8ca616a2bf375644a01e271f429a0f82736b0676
Anna-Biteleva <biteleva.anna@gmail.com>
2021-09-28 16:51:18 +0300

    My_Libs+ merge 3 lesson

---
+++ src/My_Libs/Random.java
@@ -0,0 +1,28 @@
+package My_Libs;
+
+import javax.xml.crypto.Data;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Random {
+
+   // public static Scanner scanner = new Scanner(System.in);
+
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+        randomValue();
+
+    }
+
+    public static int randomValue(int value1, int value2) {
+        if (value2 > value1) {
+            int tmp = value2 - value1;
+            return value1 + random.nextInt(tmp + 1);
+        } else {
+            System.out.println("Invalid values");
+            return -1;
+        }
+    }
+
+}
