421e80ad765191206b96214f8d0ff75eed0db8b4
Anna-Biteleva <biteleva.anna@gmail.com>
2021-10-08 18:52:50 +0300

    Homework_6

---
+++ src/HomeWork_6/Cat.java
@@ -0,0 +1,20 @@
+package HomeWork_6;
+
+public class Cat extends Animal {
+
+    public static int catNum = 0;
+
+    public Cat( int runLength, int swimLength, String name) {
+        super( runLength, swimLength,name);
+
+        catNum++ ;
+    }
+
+    @Override
+    public void swim (String name, int dist ){
+       System.out.println (name +", как и все коты, вообще не умеет плавать.");
+
+    }
+
+
+}
